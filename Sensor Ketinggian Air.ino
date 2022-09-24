6c5cf858b688927bb66becd6a0d65143a427bc5c
khoerulmutaqinkm <92683033+khoerulmutaqinkm@users.noreply.github.com>
2022-09-24 16:02:46 +0700

    Update Sensor Ketinggian Air.ino

--- Sensor Ketinggian Air.ino
+++ Sensor Ketinggian Air.ino
@@ -1,87 +1,98 @@
+#define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#define BLYNK_PRINT Serial
-#define VPIN_LED1      V2
-#define VPIN_LED2      V3 
-#define VPIN_LED3      V4
-
-
-// Authorization token from the Blynk App.
-char auth[] = "tt1tcqOkNBXMsMtMlzvRDNGpuZutSImA";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-
+//BlynkTimer timer;
+WidgetLED led1(V2);
+WidgetLED led2(V3);
+WidgetLED led3(V4);
+// Authorization token from the Blynk App. //Masukan Token Blynk
+// Your WiFi credentials.                  //Masukan Nama Wifi
+// Set password to "" for open networks.   //Password Wifi
+//#define BLYNK_TEMPLATE_ID "TMPL5GOC0ADl"
+//#define BLYNK_DEVICE_NAME "Pendeteksi Ketinggian Air"
+//#define BLYNK_AUTH_TOKEN "fr1iZLenOMmeXUQ27ysf_jmfCl0SkcDV"
+char auth[] = "fr1iZLenOMmeXUQ27ysf_jmfCl0SkcDV";
 char ssid[] = "Nabilahatha";
 char pass[] = "Abil1106";
 
-
-const int sensorPin= A0;
+//data sensor di A0 (analog 0) 
+const int sensorPin = A0;
 float liquid_level;
 int liquid_graph;
-const int Led1 = D1;
-const int Led2 = D2;
-const int Led3 = D3;
+//led 1 untuk D1
+const int led1 = D1;
+//led 2 untuk D2
+const int led2 = D2;
+//led 3 untuk D3
+const int led3 = D3;
 
-const int BatasBawah = 800;
-const int BatasTengah = 900;
-const int BatasAtas = 960;
+const int BatasBawah = 250;
+const int BatasAtas = 550;
 
 void setup()
 {
-Serial.begin(9600);
-Blynk.begin(auth, ssid, pass);
-pinMode(Led1, OUTPUT);
-pinMode(Led2, OUTPUT) ;
-pinMode(Led3, OUTPUT) ;
-pinMode(sensorPin, INPUT);
-Blynk.virtualWrite(V0, liquid_level);
-Blynk.virtualWrite(V1, liquid_graph);//This wil show the percentage of water in the container in a virtual pin V1
-Blynk.run();
-// initialize the LED pin as an output:
-pinMode(Led1, OUTPUT);
-pinMode(Led2, OUTPUT) ;
-pinMode(Led3, OUTPUT) ;
-// initialize serial communications:       
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Blynk.virtualWrite(V0, liquid_level); // This Use Basic Value  pf water container
+  Blynk.virtualWrite(V1, liquid_graph); // This wil show the percentage of water in the container in a virtual pin V1
+  Blynk.run();
+  // initialize the LED pin as an output:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  //  initialize serial communications:
 }
 
 void loop()
 {
-// read the value of the potentiometer:
-liquid_level = analogRead(sensorPin);
-liquid_graph =analogRead(sensorPin);//Percentage of water in the container 
-Serial.println(liquid_level);//This will print the liquid level in the monitor 
-Serial.println(liquid_graph);//This will print the percentage of liquid in the monitor
-Blynk.virtualWrite(V0, liquid_level);
-Blynk.virtualWrite(V1, liquid_graph);    
+  // read the value of the potentiometer:
+  liquid_level = analogRead(sensorPin);
+  liquid_graph = analogRead(sensorPin); // Percentage of water in the container
+  Serial.println("liquid_level :");
+  Serial.print(liquid_level); // This will print the liquid level in the monitor
+  Serial.println("liquid_graph :");
+  Serial.print(liquid_graph); // This will print the percentage of liquid in the monitor
+  Blynk.virtualWrite(V0, liquid_level);
+  Blynk.virtuallWrite(V1, liquid_graph);
+  Blynk.run();
+  int analogValue = analogRead(sensorPin);
+  // if the analog value is high enough, turn on the LED:
+  if (analogValue < BatasBawah)
+  {
+    led1.on();
+    digitalWrite(led1, HIGH);
+  }
+  else
+  {
+    led1.off();
+    digitalWrite(led1, LOW);
+  }
 
-Blynk.virtualWrite(VPIN_LED1,  led1);
-Blynk.virtualWrite(VPIN_LED2,  led2);
-Blynk.virtualWrite(VPIN_LED3,  led3);
+  if (analogValue > BatasBawah)
+  {
+    led2.on();
+    digitalWrite(led2, HIGH);
+  }
+  else
+  {
+    led2.off();
+    digitalWrite(led2, LOW);
+  }
 
-Blynk.run();
-int analogValue = analogRead(sensorPin);
-// if the analog value is high enough, turn on the LED:
-if (analogValue > BatasBawah)
-{
-digitalWrite(Led1, HIGH);
-digitalWrite(Led2, LOW);
-digitalWrite(Led3, LOW);
-}
-else if (analogValue > BatasTengah)
-{
-digitalWrite(Led1, LOW);
-digitalWrite(Led2, HIGH);
-digitalWrite(Led3, LOW);
-}
-else if (analogValue > BatasAtas)
-{
-digitalWrite(Led1, LOW);
-digitalWrite(Led2, LOW);
-digitalWrite(Led3, HIGH);
-}
-// print the analog value:
-Serial.println(analogValue);
-delay(100);       
-// rentang waktu pengiriman data
+  if (analogValue > BatasAtas)
+  {
+    led3();
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+  }
+  else
+  {
+    led3();
+    digitalWrite(led3, LOW);
+  }
+  delay(1000); // delay in between reads for stability
 }
